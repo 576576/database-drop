2798bfc1f3593a432da11ee74f4f229d14a33027
黄俊烨 <54106099+576576@users.noreply.github.com>
2025-03-02 15:38:05 +0800

    init code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Session, Tables } from 'koishi'
 
 export const name = 'database-drop'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('ddrop <tableName>')
+    .action(async ({ session }, tableName) => {
+      console.log(`session: ${session.type}`)
+      await ctx.database.drop(tableName as any)
+    })
 }
